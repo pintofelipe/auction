212ccf852ff93f0bd6ce8778bbfbf9b1170128d8
pintofelipe <afpintou@ufpso.edu.co>
2025-09-02 01:17:35 -0500

    feat: add useState for component only practice

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
+
 function Home() {
+  const [result, setResult] = useState(null);
+
+
+const showNumber = ()=>{
+
+  setResult(77);
+
+}
+
   return (
     <>
       <header className="flex justify-center underline hover:cursor-pointer mb-10">
         <p className="text-8xl">Auction</p>
       </header>
       <div className="grid-cols-2 grid container mx-auto">
-        <h1 className="text-6xl font-semibold bg-green-700 flex justify-center items-center">
-          Coco77
+        <h1 className="font-semibold bg-green-700 flex justify-center items-center text-9xl">
+          {result}
         </h1>
-        <h3 className="flex justify-center text-9xl font-medium bg-purple-700">
-          77
-        </h3>
+        <h1 className="flex justify-center text-9xl font-medium bg-purple-700">
+          {result}
+        </h1>
+
+        <button
+        onClick={showNumber}
+         className="bg-gray-800 text-white px-4 py-2 rounded mt-4 hover:bg-gray-600 hover:cursor-pointer mr-8"
+        >⚠️ Si quieres ver un # maravilloso haz clik aqui. ⚠️</button>
 
         <button
           onClick={() => window.history.back()}
           className="bg-gray-800 text-white px-4 py-2 rounded mt-4 hover:bg-gray-600 hover:cursor-pointer"
         >
-          Atras
+          Atrás
         </button>
       </div>
     </>
